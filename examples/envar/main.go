9334992655bbc1867de2114ce96e0c3f5c243ca4
Ivan Sukharev <isukharev@wowworks.ru>
2020-07-30 15:41:49 +0400

    Example demo ENVar

---
+++ examples/envar/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/nobuenhombre/suikat/pkg/envar"
+)
+
+type AppConfig struct {
+	Path           string  `env:"PATH:string=/some/default/path"`
+	Port           int     `env:"PORT:int=8080"`
+	Coefficient    float64 `env:"COEFFICIENT:float64=75.31"`
+	MakeSomeAction bool    `env:"MSA:bool=false"`
+}
+
+func main() {
+	cfg := &AppConfig{}
+	envar.GetENV(cfg)
+
+	fmt.Printf("%#v", cfg)
+}
