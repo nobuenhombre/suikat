d09174c81cef04042d102ffc1db0b50e6c6ca1ad
Ivan Sukharev <nobuenhombre@yandex.ru>
2020-09-09 00:29:17 +0400

    colorog package

--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/fatih/color v1.9.0
 	github.com/golang/gddo v0.0.0-20200831202555-721e228c7686 // indirect
 	github.com/gorilla/schema v1.2.0
-	github.com/klauspost/compress v1.10.11 // indirect
+	github.com/klauspost/compress v1.11.0 // indirect
 	github.com/klauspost/pgzip v1.2.5 // indirect
 	github.com/mattn/go-colorable v0.1.7 // indirect
 	github.com/mholt/archiver/v3 v3.3.0
@@ -16,5 +16,5 @@ require (
 	github.com/pierrec/lz4 v2.5.2+incompatible // indirect
 	github.com/ulikunitz/xz v0.5.8 // indirect
 	golang.org/x/net v0.0.0-20200904194848-62affa334b73 // indirect
-	golang.org/x/sys v0.0.0-20200905004654-be1d3432aa8f // indirect
+	golang.org/x/sys v0.0.0-20200908134130-d2e65c121b96 // indirect
 )
---
+++ pkg/colorog/colorog.go
@@ -0,0 +1,123 @@
+package colorog
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/fatih/color"
+)
+
+type Palette struct {
+	TimeColor    *color.Color
+	SuccessColor *color.Color
+	InfoColor    *color.Color
+	MessageColor *color.Color
+	ErrorColor   *color.Color
+	FatalColor   *color.Color
+	PanicColor   *color.Color
+}
+
+type ColoredLog struct {
+	Palette
+}
+
+func NewColoredLog() *ColoredLog {
+	coloredLog := &ColoredLog{
+		Palette: Palette{
+			TimeColor:    color.New(color.FgMagenta),
+			SuccessColor: color.New(color.FgGreen),
+			InfoColor:    color.New(color.FgCyan),
+			MessageColor: color.New(color.FgWhite),
+			ErrorColor:   color.New(color.FgYellow),
+			FatalColor:   color.New(color.FgRed),
+			PanicColor:   color.New(color.FgWhite, color.BgRed),
+		},
+	}
+
+	// Change Log Format
+	log.SetFlags(0)
+	log.SetOutput(coloredLog)
+
+	return coloredLog
+}
+
+func (cl *ColoredLog) Write(bytes []byte) (int, error) {
+	return fmt.Printf(
+		"%v %v",
+		cl.TimeColor.Sprint(time.Now().UTC().Format("2006-01-02 15:04:05")),
+		string(bytes),
+	)
+}
+
+func (cl *ColoredLog) Success(v ...interface{}) {
+	log.Print(cl.SuccessColor.Sprint(v...))
+}
+
+func (cl *ColoredLog) Successf(format string, v ...interface{}) {
+	log.Print(cl.SuccessColor.Sprintf(format, v...))
+}
+
+func (cl *ColoredLog) Successln(v ...interface{}) {
+	log.Println(cl.SuccessColor.Sprint(v...))
+}
+
+func (cl *ColoredLog) Error(v ...interface{}) {
+	log.Print(cl.ErrorColor.Sprint(v...))
+}
+
+func (cl *ColoredLog) Errorf(format string, v ...interface{}) {
+	log.Print(cl.ErrorColor.Sprintf(format, v...))
+}
+
+func (cl *ColoredLog) Errorln(v ...interface{}) {
+	log.Println(cl.ErrorColor.Sprint(v...))
+}
+
+func (cl *ColoredLog) Info(v ...interface{}) {
+	log.Print(cl.InfoColor.Sprint(v...))
+}
+
+func (cl *ColoredLog) Infof(format string, v ...interface{}) {
+	log.Print(cl.InfoColor.Sprintf(format, v...))
+}
+
+func (cl *ColoredLog) Infoln(v ...interface{}) {
+	log.Println(cl.InfoColor.Sprint(v...))
+}
+
+func (cl *ColoredLog) Message(v ...interface{}) {
+	log.Print(cl.MessageColor.Sprint(v...))
+}
+
+func (cl *ColoredLog) Messagef(format string, v ...interface{}) {
+	log.Print(cl.MessageColor.Sprintf(format, v...))
+}
+
+func (cl *ColoredLog) Messageln(v ...interface{}) {
+	log.Println(cl.MessageColor.Sprint(v...))
+}
+
+func (cl *ColoredLog) Fatal(v ...interface{}) {
+	log.Fatal(cl.FatalColor.Sprint(v...))
+}
+
+func (cl *ColoredLog) Fatalf(format string, v ...interface{}) {
+	log.Fatal(cl.FatalColor.Sprintf(format, v...))
+}
+
+func (cl *ColoredLog) Fatalln(v ...interface{}) {
+	log.Fatalln(cl.FatalColor.Sprint(v...))
+}
+
+func (cl *ColoredLog) Panic(v ...interface{}) {
+	log.Panic(cl.PanicColor.Sprint(v...))
+}
+
+func (cl *ColoredLog) Panicf(format string, v ...interface{}) {
+	log.Panic(cl.PanicColor.Sprintf(format, v...))
+}
+
+func (cl *ColoredLog) Panicln(v ...interface{}) {
+	log.Panicln(cl.PanicColor.Sprint(v...))
+}
