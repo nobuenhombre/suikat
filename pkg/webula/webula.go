a94cc7d6216114b4e84b209d7be057bd2cf82ad8
Ivan Sukharev <isukharev@wowworks.ru>
2020-11-18 15:57:40 +0400

    add new func webula.TranslitRusLat()

--- pkg/webula/webula.go
+++ pkg/webula/webula.go
@@ -206,3 +206,82 @@ func NormalizeAlphabet(name string) string {
 
 	return strings.Join(phrases, Comma+Space)
 }
+
+func TranslitRusLat(in string) string {
+	out := in
+
+	translitReplaces := map[string]string{
+		"А": "A",
+		"Б": "B",
+		"В": "V",
+		"Г": "G",
+		"Д": "D",
+		"Е": "E",
+		"Ё": "YO",
+		"Ж": "ZH",
+		"З": "Z",
+		"И": "I",
+		"Й": "Y",
+		"К": "K",
+		"Л": "L",
+		"М": "M",
+		"Н": "N",
+		"О": "O",
+		"П": "P",
+		"Р": "R",
+		"С": "S",
+		"Т": "T",
+		"У": "U",
+		"Ф": "F",
+		"Х": "H",
+		"Ц": "C",
+		"Ч": "CH",
+		"Ш": "SH",
+		"Щ": "SCH",
+		"Ъ": "",
+		"Ы": "Y",
+		"Ь": "",
+		"Э": "E",
+		"Ю": "YU",
+		"Я": "YA",
+		"а": "a",
+		"б": "b",
+		"в": "v",
+		"г": "g",
+		"д": "d",
+		"е": "e",
+		"ё": "yo",
+		"ж": "zh",
+		"з": "z",
+		"и": "i",
+		"й": "y",
+		"к": "k",
+		"л": "l",
+		"м": "m",
+		"н": "n",
+		"о": "o",
+		"п": "p",
+		"р": "r",
+		"с": "s",
+		"т": "t",
+		"у": "u",
+		"ф": "f",
+		"х": "h",
+		"ц": "c",
+		"ч": "ch",
+		"ш": "sh",
+		"щ": "sch",
+		"ъ": "",
+		"ы": "y",
+		"ь": "",
+		"э": "e",
+		"ю": "yu",
+		"я": "ya",
+	}
+
+	for rus, lat := range translitReplaces {
+		out = strings.ReplaceAll(out, rus, lat)
+	}
+
+	return out
+}
