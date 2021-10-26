584550ccf36f453181c62538e7368dc71683a210
ivan.sukharev <isukharev@wowworks.ru>
2021-10-26 16:03:40 +0400

    Add Duration in SQL logger

--- pkg/db/types/types.go
+++ pkg/db/types/types.go
@@ -1,10 +1,12 @@
 package types
 
+import "time"
+
 // Log provides the log func used by generated queries.
-type SQLLoggerFunc func(sql string, sqlParams ...interface{})
+type SQLLoggerFunc func(sql string, du time.Duration, sqlParams ...interface{})
 
 type SQLLogger interface {
-	WriteLog(sql string, sqlParams ...interface{})
+	WriteLog(sql string, du time.Duration, sqlParams ...interface{})
 }
 
 type DBConfig interface {
@@ -15,6 +17,6 @@ type DBLog struct {
 	Log SQLLoggerFunc
 }
 
-func (dbl *DBLog) WriteLog(sql string, sqlParams ...interface{}) {
-	dbl.Log(sql, sqlParams...)
+func (dbl *DBLog) WriteLog(sql string, du time.Duration, sqlParams ...interface{}) {
+	dbl.Log(sql, du, sqlParams...)
 }
