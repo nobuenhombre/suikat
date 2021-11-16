b98b90d64a9abf04797fe01a7436fcfcc5ed0214
ivan.sukharev <isukharev@wowworks.ru>
2021-11-16 12:57:06 +0400

    Add func StringToIntSlice

--- pkg/converter/converter.go
+++ pkg/converter/converter.go
@@ -2,7 +2,9 @@ package converter
 
 import (
 	"fmt"
+	"github.com/nobuenhombre/suikat/pkg/ge"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -19,11 +21,11 @@ func (e *ParserError) Error() string {
 func StringToInt(s string) (int, error) {
 	n, err := strconv.Atoi(s)
 	if err != nil {
-		return 0, &ParserError{
+		return 0, ge.Pin(&ParserError{
 			ParserType: "Int",
 			Value:      s,
 			Parent:     err,
-		}
+		})
 	}
 
 	return n, nil
@@ -33,11 +35,11 @@ func StringToInt8(s string) (int8, error) {
 	n, err := strconv.ParseInt(s, 10, 8)
 
 	if err != nil {
-		return 0, &ParserError{
+		return 0, ge.Pin(&ParserError{
 			ParserType: "Int8",
 			Value:      s,
 			Parent:     err,
-		}
+		})
 	}
 
 	return int8(n), nil
@@ -47,11 +49,11 @@ func StringToInt16(s string) (int16, error) {
 	n, err := strconv.ParseInt(s, 10, 16)
 
 	if err != nil {
-		return 0, &ParserError{
+		return 0, ge.Pin(&ParserError{
 			ParserType: "Int16",
 			Value:      s,
 			Parent:     err,
-		}
+		})
 	}
 
 	return int16(n), nil
@@ -61,11 +63,11 @@ func StringToInt32(s string) (int32, error) {
 	n, err := strconv.ParseInt(s, 10, 32)
 
 	if err != nil {
-		return 0, &ParserError{
+		return 0, ge.Pin(&ParserError{
 			ParserType: "Int32",
 			Value:      s,
 			Parent:     err,
-		}
+		})
 	}
 
 	return int32(n), nil
@@ -75,11 +77,11 @@ func StringToInt64(s string) (int64, error) {
 	n, err := strconv.ParseInt(s, 10, 64)
 
 	if err != nil {
-		return 0, &ParserError{
+		return 0, ge.Pin(&ParserError{
 			ParserType: "Int64",
 			Value:      s,
 			Parent:     err,
-		}
+		})
 	}
 
 	return n, nil
@@ -89,11 +91,11 @@ func StringToBool(s string) (bool, error) {
 	b, err := strconv.ParseBool(s)
 
 	if err != nil {
-		return false, &ParserError{
+		return false, ge.Pin(&ParserError{
 			ParserType: "Bool",
 			Value:      s,
 			Parent:     err,
-		}
+		})
 	}
 
 	return b, nil
@@ -103,11 +105,11 @@ func StringToFloat32(s string) (float32, error) {
 	f, err := strconv.ParseFloat(s, 32)
 
 	if err != nil {
-		return 0, &ParserError{
+		return 0, ge.Pin(&ParserError{
 			ParserType: "Float32",
 			Value:      s,
 			Parent:     err,
-		}
+		})
 	}
 
 	return float32(f), nil
@@ -117,11 +119,11 @@ func StringToFloat64(s string) (float64, error) {
 	f, err := strconv.ParseFloat(s, 64)
 
 	if err != nil {
-		return 0, &ParserError{
+		return 0, ge.Pin(&ParserError{
 			ParserType: "Float64",
 			Value:      s,
 			Parent:     err,
-		}
+		})
 	}
 
 	return f, nil
@@ -131,12 +133,31 @@ func StringToTime(s, format string) (time.Time, error) {
 	t, err := time.Parse(format, s)
 
 	if err != nil {
-		return time.Time{}, &ParserError{
+		return time.Time{}, ge.Pin(&ParserError{
 			ParserType: "time.Time",
 			Value:      s,
 			Parent:     err,
-		}
+		})
 	}
 
 	return t, nil
+}
+
+func StringToIntSlice(s, sep string) ([]int, error) {
+	sList := strings.Split(s, sep)
+	out := make([]int, len(sList))
+	for i, sValue := range sList {
+		iValue, err := StringToInt(sValue)
+		if err != nil {
+			return []int{}, ge.Pin(&ParserError{
+				ParserType: "IntSlice",
+				Value:      fmt.Sprintf("s=%v sep=%v index=%v", s, sep, i),
+				Parent:     err,
+			})
+		}
+
+		out = append(out, iValue)
+	}
+
+	return out, nil
 }
